a1daa1c211d13432c5a9a73733ef90f85ca58d9e
eric chang <eric.chang@izenda.com>
2020-05-18 12:06:03 -0400

    update

--- HtmlStarterKitCore_BE_Standalone/Website/izenda.integrate.js
+++ HtmlStarterKitCore_BE_Standalone/Website/izenda.integrate.js
@@ -46,14 +46,8 @@ var DoRender = function (successFunc) {
 };
 
 var izendaInit = function () {
-    //function successFunc(data, status) {
-    //var currentUserContext = {
-    //    //token: data.token
-    //    token: localStorage.getItem("token")
-    //};
     //Works with localstorage
     var tokenFromStorage = localStorage.getItem('token');
-    //var currentUserContext = document.cookie;
     var currentUserContext = {
         token: tokenFromStorage
     };
@@ -61,9 +55,6 @@ var izendaInit = function () {
         console.log(currentUserContext);
         IzendaSynergy.setCurrentUserContext(currentUserContext);
         IzendaSynergy.render(document.getElementById('izenda-root'));
-    //}
-
-    //this.DoRender(successFunc);
 
 };
 
@@ -83,18 +74,6 @@ var izendaInitReport = function () {
 };
 
 var izendaInitSetting = function () {
-  //function successFunc(data, status) {
-    //    console.info(data);
-    //    var currentUserContext = {
-    //        token: data.token
-    //    };
-
-    //    IzendaSynergy.setCurrentUserContext(currentUserContext);
-    //    IzendaSynergy.renderSettingPage(document.getElementById('izenda-root'));
-    //}
-
-    //this.DoRender(successFunc);
-
     var tokenFromStorage = localStorage.getItem('token');
     var currentUserContext = {
         token: tokenFromStorage
@@ -170,31 +149,27 @@ var izendaRenderReportPart = function (reportPartId, container) {
 
 var izendaInitReport = function () {
 
-    function successFunc(data, status) {
-        var currentUserContext = {
-            token: data.token
-        };
+    var tokenFromStorage = localStorage.getItem('token');
+    var currentUserContext = {
+        token: tokenFromStorage
+    };
 
-        IzendaSynergy.setCurrentUserContext(currentUserContext);
-        IzendaSynergy.renderReportPage(document.getElementById('izenda-root'));
-    }
-
-    this.DoRender(successFunc);
-
+    console.log(currentUserContext);
+    IzendaSynergy.setCurrentUserContext(currentUserContext);
+    IzendaSynergy.renderReportPage(document.getElementById('izenda-root'));
 };
 
 // Render report viewer to a <div> tag by report id
 var izendaInitReportViewer = function (reportId, filters) {
-    function successFunc(data, status) {
-        var currentUserContext = {
-            token: data.token
-        };
-        IzendaSynergy.setCurrentUserContext(currentUserContext);
-        IzendaSynergy.renderReportViewerPage(document.getElementById('izenda-root'), reportId, filters);
-    }
 
-    this.DoRender(successFunc);
+    var tokenFromStorage = localStorage.getItem('token');
+    var currentUserContext = {
+        token: tokenFromStorage
+    };
 
+    console.log(currentUserContext);
+    IzendaSynergy.setCurrentUserContext(currentUserContext);
+    IzendaSynergy.renderReportViewerPage(document.getElementById('izenda-root'), reportId, filters);
 };
 
 var izendaInitReportCustomFilters = function (reportObject) {
@@ -219,61 +194,51 @@ var izendaInitReportCustomFilters = function (reportObject) {
 
 var izendaInitDashboard = function () {
 
-    function successFunc(data, status) {
-        var currentUserContext = {
-            token: data.token
-        };
+    var tokenFromStorage = localStorage.getItem('token');
+    var currentUserContext = {
+        token: tokenFromStorage
+    };
 
-        IzendaSynergy.setCurrentUserContext(currentUserContext);
-        IzendaSynergy.renderDashboardPage(document.getElementById('izenda-root'));
-    }
-
-    this.DoRender(successFunc);
-
+    console.log(currentUserContext);
+    IzendaSynergy.setCurrentUserContext(currentUserContext);
+    IzendaSynergy.renderDashboardPage(document.getElementById('izenda-root'));
 };
 
 // Render dashboard viewer to a <div> tag by dashboard id
 var izendaInitDashboardViewer = function (dashboardId, filters) {
-    function successFunc(data, status) {
-        var currentUserContext = {
-            token: data.token
-        };
-        IzendaSynergy.setCurrentUserContext(currentUserContext);
-        IzendaSynergy.renderDashboardViewerPage(document.getElementById('izenda-root'), dashboardId, filters);
-    }
 
-    this.DoRender(successFunc);
+    var tokenFromStorage = localStorage.getItem('token');
+    var currentUserContext = {
+        token: tokenFromStorage
+    };
+
+    console.log(currentUserContext);
+    IzendaSynergy.setCurrentUserContext(currentUserContext);
+    IzendaSynergy.renderDashboardViewerPage(document.getElementById('izenda-root'), dashboardId, filters);
 };
 
 var izendaInitReportDesigner = function () {
 
-    function successFunc(data, status) {
-        var currentUserContext = {
-            token: data.token
-        };
+    var tokenFromStorage = localStorage.getItem('token');
+    var currentUserContext = {
+        token: tokenFromStorage
+    };
 
-        IzendaSynergy.setCurrentUserContext(currentUserContext);
-        IzendaSynergy.renderReportDesignerPage(document.getElementById('izenda-root'));
-
-    }
-
-    this.DoRender(successFunc);
-
+    console.log(currentUserContext);
+    IzendaSynergy.setCurrentUserContext(currentUserContext);
+    IzendaSynergy.renderReportDesignerPage(document.getElementById('izenda-root'));
 };
 
 var izendaInitNewDashboard = function () {
 
-    function successFunc(data, status) {
-        var currentUserContext = {
-            token: data.token
-        };
+    var tokenFromStorage = localStorage.getItem('token');
+    var currentUserContext = {
+        token: tokenFromStorage
+    };
 
-        IzendaSynergy.setCurrentUserContext(currentUserContext);
-        IzendaSynergy.renderNewDashboardPage(document.getElementById('izenda-root'));
-    }
-
-    this.DoRender(successFunc);
-
+    console.log(currentUserContext);
+    IzendaSynergy.setCurrentUserContext(currentUserContext);
+    IzendaSynergy.renderNewDashboardPage(document.getElementById('izenda-root'));
 };
 
 var izendaInitReportPartExportViewer = function (reportPartId, token) {
@@ -286,4 +251,37 @@ var izendaInitReportPartExportViewer = function (reportPartId, token) {
         useQueryParam: true,
         useHash: false
     });
+};
+
+var izendaInitReportPartDemo = function() {
+
+  var tokenFromStorage = localStorage.getItem('token');
+  var currentUserContext = {
+      token: tokenFromStorage
+  };
+
+  // You can add report parts after creating reports using the context below
+  // Add the report part ID's in the <reportPartIds> array
+  var reportPartIds = new Array(
+    "D2647E59-AEB8-48B9-A228-28E8CD96D622",
+    "D2647E59-AEB8-48B9-A228-28E8CD96D622",
+    "D2647E59-AEB8-48B9-A228-28E8CD96D622",
+    "D2647E59-AEB8-48B9-A228-28E8CD96D622"
+  );
+
+  IzendaSynergy.setCurrentUserContext(currentUserContext);
+
+  for (var i = 0; i < reportPartIds.length; i++) {
+    //create report part container
+    var reportPartNode = document.createElement("div");
+    reportPartNode.className = "report-part-chart";
+
+    //Render report part
+    IzendaSynergy.renderReportPart(reportPartNode, {
+      "id": reportPartIds[i]
+    });
+
+    //Append to izenda-root container
+    document.getElementById('izenda-root').appendChild(reportPartNode);
+  }
 };
