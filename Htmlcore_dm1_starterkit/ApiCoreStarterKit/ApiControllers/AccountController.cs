90795138c0836579f2ca2bf10f3a080e76d9e358
eric chang <eric.chang@izenda.com>
2020-06-23 10:40:46 -0400

    renaming

---
+++ Htmlcore_dm1_starterkit/ApiCoreStarterKit/ApiControllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using ApiCoreStarterKit.Models;
+using Mvc5StarterKit.IzendaBoundary;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCoreStarterKit.ApiControllers
+{
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        #region Methods
+        [HttpGet]
+        [Route("validateIzendaAuthToken")]
+        public UserInfo ValidateIzendaAuthToken(string access_token)
+        {
+            var userInfo = IzendaTokenAuthorization.GetUserInfo(access_token);
+            Console.WriteLine(userInfo);
+
+            return userInfo;
+        }
+
+        [HttpGet]
+        [Route("GetIzendaAccessToken")]
+        public ActionResult<string> GetIzendaAccessToken(string message)
+        {
+            var userInfo = IzendaTokenAuthorization.DecryptIzendaAuthenticationMessage(message);
+            var token = IzendaTokenAuthorization.GetToken(userInfo);
+
+            return Ok(new { Token = token });
+        }
+        #endregion
+    }
+}
+
+// If you use .Net Framework, please replace with following codes.
+// Dot Net Core is not supposed to use a controller inherited from ApiController (System.Web.Http) but ControllerBase (Microsoft.AspNetCore.Mvc).
+// HttpGet, Route follow the same rule.
+/*
+using ApiCoreStarterKit.Models;
+using Mvc5StarterKit.IzendaBoundary;
+using System;
+using System.Web.Http;
+
+namespace ApiCoreStarterKit.ApiControllers
+{
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiController
+    {
+        #region Methods
+        [HttpGet]
+        [Route("validateIzendaAuthToken")]
+        public UserInfo ValidateIzendaAuthToken(string access_token)
+        {
+            var userInfo = IzendaTokenAuthorization.GetUserInfo(access_token);
+
+            return userInfo;
+        }
+
+        [HttpGet]
+        [Route("GetIzendaAccessToken")]
+        public IHttpActionResult GetIzendaAccessToken(string message)
+        {
+            var userInfo = IzendaTokenAuthorization.DecryptIzendaAuthenticationMessage(message);
+            var token = IzendaTokenAuthorization.GetToken(userInfo);
+
+            return Ok(token);
+        }
+        #endregion
+    }
+}*/
---
+++ Htmlcore_dm1_starterkit/ApiCoreStarterKit/Controllers/TenantController.cs
@@ -0,0 +1,82 @@
+﻿using ApiCoreStarterKit.IzendaBoundary;
+using ApiCoreStarterKit.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Mvc5StarterKit.IzendaBoundary;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiCoreStarterKit.Controllers
+{
+    [Route("api/tenant")]
+    public class TenantController : BaseController
+    {
+        #region Variables
+        private readonly IConfiguration _configuration;
+        #endregion
+
+        #region CTOR
+        public TenantController(IConfiguration configuration) => _configuration = configuration;
+        #endregion
+
+        #region Methods
+        [EnableCors("AllowOrigin")]
+        [HttpGet]
+        [Route("CreateTenant")]
+        public async Task<JsonResult> CreateTenant(string tenantID, string tenantName)
+        {
+            if (string.IsNullOrEmpty(tenantID) || string.IsNullOrEmpty(tenantName))
+                return AddJsonResult(false);
+            else
+            {
+                var izendaAdminAuthToken = IzendaTokenAuthorization.GetIzendaAdminToken();
+                var connectString = _configuration.GetConnectionString("DefaultConnection");
+
+                var isTenantExist = IzendaUtilities.GetTenantByName(tenantID, connectString); // check user DB first
+
+                if (isTenantExist == null)
+                {
+                    var success = await IzendaUtilities.CreateTenant(tenantName, tenantID, izendaAdminAuthToken);
+
+                    if (success)
+                    {
+                        // save a new tenant at user DB
+                        var newTenant = new Tenant() { Name = tenantID };
+                        await IzendaUtilities.SaveTenantAsync(newTenant, connectString);
+
+                        return AddJsonResult(true);
+                    }
+                    else
+                        return AddJsonResult(false);
+                }
+                else
+                    return AddJsonResult(false);
+            }
+        }
+
+        [EnableCors("AllowOrigin")]
+        [HttpGet]
+        [Route("GetTenantList")]
+        public string GetTenantList()
+        {
+            // todo
+            var connectString = _configuration.GetConnectionString("DefaultConnection");
+            var tenantList = IzendaUtilities.GetAllTenants(connectString);
+
+            var list = new List<string>();
+            list.Add("Select Tenant");
+
+            foreach (var tenant in tenantList)
+            {
+                list.Add(tenant.Name);
+            }
+
+            var result = JsonConvert.SerializeObject(list);
+
+            return result;
+        }
+        #endregion
+    }
+}
