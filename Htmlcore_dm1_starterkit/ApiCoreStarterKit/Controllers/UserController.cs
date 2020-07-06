0ec954afaed1e3f18fb39744c9c6ef2055a405d6
eric chang <eric.chang@izenda.com>
2020-07-06 16:20:23 -0400

    update

--- Htmlcore_dm1_starterkit/ApiCoreStarterKit/Controllers/UserController.cs
+++ Htmlcore_dm1_starterkit/ApiCoreStarterKit/Controllers/UserController.cs
@@ -29,7 +29,7 @@ namespace ApiCoreStarterKit.Controllers
         [Route("GenerateToken")]
         public JsonResult GenerateToken(string tenant, string email, string password)
         {
-            bool.TryParse(_configuration.GetValue<string>("useADlogin:useADlogin"), out bool useADlogin); // if you want to enable active directory login, then set this boolean value to true (appsettings.json). Default is false.
+            bool.TryParse(_configuration.GetValue<string>("useADlogin"), out bool useADlogin); // if you want to enable active directory login, then set this boolean value to true (appsettings.json). Default is false.
 
             var defaultConnectionString = _configuration.GetConnectionString("DefaultConnection");
             var success = false;
